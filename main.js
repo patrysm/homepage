62696c98ec33a6217d1076c596a9c82b9cfbecbe
Patryk Michalak <patrykmichalak@gmail.com>
2020-05-20 23:28:11 +0200

    wysrodkowanie_nawigacji_flexboxem

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const firstName = "Patryk";
 const number = 600;
+const age = 31;
 
 console.log(`Siema! Jestem ${firstName}, witam na mojej stronie :)`);
 console.log(`Pozdro ${number}!`);
 
 const heading = document.querySelector(".main__heading--js");
 heading.innerHTML = `Mam na imię ${firstName}.`;
+
+
+function greet(age, firstName) {
+    
+}
